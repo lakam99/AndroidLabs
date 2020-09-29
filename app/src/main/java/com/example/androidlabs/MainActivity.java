51f036975e671b4f367dcb10e822e33c309ab841
Arkam Mazrui <superarkam@gmail.com>
2020-09-28 22:33:33 -0400

    finished step 2 (forreal)

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
 
         final Switch my_switch = findViewById(R.id.my_switch);
 
